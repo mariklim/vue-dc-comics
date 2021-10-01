d667ff5f5bbdb2b1a3e96ab3a63a2377dd6b66d8
mariklim <maria.klimenko889@gmail.com>
2021-10-01 09:11:10 +0200

    add json file

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <img :src="info.thumb" alt="info.type">
+      <p>{{info.series}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+    props:["info"]
+}
+</script>
+
+<style>
+
+</style>
